a2aed0fe2e5e56ae2ba58763f7e6dbf74a9f843f
ordinaryA <353458582@qq.com>
2019-10-31 19:20:24 +0800

    edit

---
+++ email.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+git filter-branch --env-filter '
+
+OLD_EMAIL="alan.l@tc-gaming.co"
+CORRECT_NAME="ordinaryA"
+CORRECT_EMAIL="353458582@qq.com"
+
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
