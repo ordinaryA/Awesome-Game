60136c7c7e31c5019dd4ed4a6a99dd5dc37953ff
ordinaryA <353458582@qq.com>
2019-12-09 11:36:36 +0800

    deploy

--- src/page/gold/index.vue
+++ src/page/gold/index.vue
@@ -7,7 +7,9 @@
             class="paper"
             v-first="{name:'translateRight',duration:'0.4s',delay:'0.5s',offset:'0'}"
           >
-            <div class="wohu">111</div>
+            <svg width="100" height="100" version="1.1" xmlns="http://www.w3.org/2000/svg">
+              <line x1="0" y1="0" x2="100%" y2="100%" style="stroke:rgb(99,99,99);stroke-width:2" />
+            </svg>
           </div>
         </div>
       </div>
@@ -23,7 +25,9 @@ import { COMMIT } from "../../utils";
 export default {
   mixins: [$animate],
   data() {
-    return {};
+    return {
+      hookPos: [100, 100]
+    };
   },
   computed: {},
   methods: {}
