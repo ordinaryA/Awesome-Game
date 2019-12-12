7dac51bc85cd45984c8d9356ea45bb490b963b27
ordinaryA <353458582@qq.com>
2019-12-12 19:47:56 +0800

    deploy

--- src/page/gold/index.vue
+++ src/page/gold/index.vue
@@ -22,18 +22,22 @@ import { mapState } from "vuex";
 import { $animate } from "../../mixins";
 import { COMMIT } from "../../utils";
 
+// 绳子默认长度
+const LINE_DEFAULT_LENGTH = 30;
+
 export default {
   mixins: [$animate],
   data() {
     return {
-      lineLength: 30, //绳子长度
+      lineLength: LINE_DEFAULT_LENGTH, //绳子长度
       lineAngle: -90, //绳子与Y轴角度
       hookStartPos: [683, 0], //绳子起点坐标
-      hookPos: [683, 30], //钩子坐标
+      hookPos: [683, LINE_DEFAULT_LENGTH], //钩子坐标
       hookTimer: null, //钩子计时器
       hookIsRotate: true, //钩子是否正在旋转
       grabTimer: null, //钩子抓出计时器
-      hookIsGrab: false //钩子是否正在抓取
+      hookIsGrab: false, //钩子是否正在抓取
+      lineIsShorting: false //绳子正在收缩
     };
   },
   created() {
@@ -62,24 +66,45 @@ export default {
      */
     rotateHooks() {
       this.hookTimer = setInterval(() => {
-        //1.0 判断钩子长度大于某长度后缩短
-        if (this.lineLength > 100) {
-          this.lineLength = 30;
-          this.hookIsGrab = false;
-          this.hookIsRotate = true;
-        }
+        // 计算钩子坐标
+        this.computHooksPos();
         //1.0 判断钩子是否正在旋转
         if (this.hookIsRotate) {
+          // 旋转时增加角度，设置不能抓取
           this.lineAngle += 1;
-          // 旋转时设置不能抓取
           this.hookIsGrab = false;
+          return;
         }
         //2.0 判断钩子是否正在抓取
         if (this.hookIsGrab) {
-          this.lineLength += 3;
+          //2.1 判断钩子长度大于某长度后缩短
+          this.lineIsShorten();
+          if (this.lineLength > 200) {
+            this.lineIsShorting = true;
+          } else {
+            this.lineLength += 3;
+          }
+          return;
         }
-        this.computHooksPos();
       }, 30);
+    },
+
+    /**
+     * 绳子缩短到正常长度
+     * @param {void}
+     * @returns {void}
+     */
+    lineIsShorten() {
+      // 如果绳子收缩状态为true时
+      if (this.lineIsShorting) {
+        this.lineLength -= 5;
+        if (this.lineLength < LINE_DEFAULT_LENGTH) {
+          // 恢复正常绳子长度，钩子恢复旋转，绳子收缩状态为false
+          this.lineLength = LINE_DEFAULT_LENGTH;
+          this.hookIsRotate = true;
+          this.lineIsShorting = false;
+        }
+      }
     },
 
     /**
