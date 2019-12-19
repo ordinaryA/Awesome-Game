356d4d6382220f313d6026c921f4e44ae8be47a9
ordinaryA <353458582@qq.com>
2019-12-19 18:49:24 +0800

    deploy

--- src/constants/gold.js
+++ src/constants/gold.js
@@ -2,38 +2,38 @@
 // 物品属性
 const 很多很多东西 = {
   篮球1: {
-    width: 80,
-    height: 80,
+    width: 150,
+    height: 150,
     price: 500,
     className: "gold_1"
   },
   篮球2: {
-    width: 50,
-    height: 50,
+    width: 120,
+    height: 120,
     price: 250,
     className: "gold_2"
   },
   篮球3: {
-    width: 30,
-    height: 30,
+    width: 80,
+    height: 80,
     price: 100,
     className: "gold_3"
   },
   篮球4: {
-    width: 15,
-    height: 15,
+    width: 50,
+    height: 50,
     price: 50,
     className: "gold_4"
   },
   头发1: {
-    width: 30,
-    height: 30,
+    width: 80,
+    height: 48,
     price: 75,
     className: "stone_1"
   },
   头发2: {
-    width: 20,
-    height: 20,
+    width: 50,
+    height: 30,
     price: 25,
     className: "stone_2"
   },
