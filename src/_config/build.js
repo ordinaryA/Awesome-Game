8359b9891a4d2e2501bb50c6980ae54d3b4b843b
ordinaryA <353458582@qq.com>
2020-09-30 18:02:39 +0800

    解决动画延迟bug

--- src/_config/build.js
+++ src/_config/build.js
@@ -1,17 +1,20 @@
 import '@style/alan.css';
 import '@style/paper.css';
-import 'animate.css';
+// 使用wow下的animate css 解决延迟BUG
+// import 'animate.css';
+import 'wowjs/css/libs/animate.css';
 
 // this上挂载的方法
 import FUNCTION from "../utils/test";
 
-//自定义指令
 import directive from '../utils/directive';
 const InstallFunction = _Vue => {
   for (const I in FUNCTION) {
     _Vue.prototype[I] = FUNCTION[I];
   }
-};;
+};
+
+//自定义指令
 const InstallCommand = _Vue => {
   //拖动
   for (const I in directive) {
