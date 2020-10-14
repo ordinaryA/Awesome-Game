4c85549d3e6324d331bb19e057498d192304b8d8
ordinaryA <353458582@qq.com>
2020-10-14 17:16:34 +0800

    平衡疯狂难度

--- src/constants/sweep.js
+++ src/constants/sweep.js
@@ -1,26 +1,27 @@
 const sidebar = [{
   title: '游戏难度',
-  children: [{
-    type: 'difficulty',
-    mode: 'normal',
-    label: '普通 (Normal)'
-  },
-  {
-    type: 'difficulty',
-    mode: 'hard',
-    label: '困难 (Hard)'
-  },
-  {
-    type: 'difficulty',
-    mode: 'crazy',
-    label: '疯狂 (Crazy)'
-  },
-  {
-    type: 'difficulty',
-    mode: 'hell',
-    label: '?????(?????)'
-  }
-  ]
+  children: [
+    {
+      type: 'difficulty',
+      mode: 'normal',
+      label: '普通 (Normal)'
+    },
+    {
+      type: 'difficulty',
+      mode: 'hard',
+      label: '困难 (Hard)'
+    },
+    {
+      type: 'difficulty',
+      mode: 'crazy',
+      label: '疯狂 (Crazy)'
+    },
+    {
+      type: 'difficulty',
+      mode: 'hell',
+      label: '?????(?????)'
+    }
+  ],
 },
 {
   title: '自定义',
@@ -33,14 +34,14 @@ const sidebar = [{
     label: '雷的数量 (Mine)'
   }
   ]
-}
+},
 ];
 
 // 游戏难度雷以及棋盘枚举
 const modeEnum = {
   normal: { boardSize: [10, 10], mineCount: 10, type: 'primary' },
   hard: { boardSize: [20, 15], mineCount: 50, type: 'secondary' },
-  crazy: { boardSize: [40, 20], mineCount: 200, type: 'warning' },
+  crazy: { boardSize: [40, 20], mineCount: 150, type: 'warning' },
   hell: { boardSize: [45, 25], mineCount: 230, type: 'danger' },
 };
 
